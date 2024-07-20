3f50747753262c234b45ad5458ee649300a8a4d4
Zayden <adriangota2007@gmail.com>
2024-07-20 19:44:47 +0700

    feat: minify all code in one

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,43 @@
 import typescript from 'rollup-plugin-typescript2';
+import deletePlugin from 'rollup-plugin-delete';
 import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 export default [
     {
         input: './src/index.ts',
-        output: {
-            file: './dist/index.mjs',
-            format: 'esm',
-        },
+        output: [
+            {
+                file: './dist/index.min.mjs',
+                format: 'esm',
+            },
+            {
+                file: './dist/index.min.cjs',
+                format: 'cjs',
+            }
+        ],
+        preserveModules: true,
         plugins: [
-            typescript({ tsconfig: './tsconfig.json' }),
+            typescript({
+                tsconfig: './tsconfig.json',
+                clean: true
+            }),
             terser()
         ]
     },
     {
         input: './src/index.ts',
         output: {
-            file: './dist/index.cjs',
-            format: 'cjs',
+            file: './dist/index.min.d.ts',
+            format: 'es',
         },
         plugins: [
-            typescript({ tsconfig: './tsconfig.json' }),
-            terser()
+            dts(),
+            deletePlugin({
+                targets: ['dist/*', '!dist/index.min.cjs', '!dist/index.min.mjs', '!dist/index.min.d.ts'],
+                hook: 'buildEnd',
+                verbose: true
+            })
         ]
     }
 ];
--- src/script/download/aio.ts
+++ src/script/download/aio.ts
@@ -1,25 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
+import formData from "form-data";
 
-interface AioResponse {
-    url: string;
-}
-
-export default async function aio(url: string): Promise<string> {
-    try {
-        const response = await axios.post<AioResponse>(
-            "https://api.cobalt.tools/api/json",
-            { url },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                }
-            }
-        );
-
-        return response.data.url;
-    } catch (error) {
-        console.error("Failed to fetch Aio data:", error);
-        throw new Error("Failed to fetch Aio data. Please try again later.");
-    }
+export default async function aio(url: string): Promise<{ url: string, size: string }[]> {
+    return new Promise(async (resolve, reject) => {
+        const form = new formData();
+        form.append("url", url);
+        await axios({
+            url: "https://www.aiodownloader.in/wp-json/aio-dl/video-data",
+            method: "post",
+            data: form
+        }).then(({ data }) => {
+            const res: PromiseLike<{ url: string; size: string; }[]> | { url: any; size: any; }[] = []
+            data.medias.map((e: { url: any; formattedSize: any; }) => {
+                res.push({
+                    url: e.url,
+                    size: e.formattedSize
+                })
+            })
+            resolve(res)
+        }).catch(() => {
+            reject("Failed to download")
+        })
+    })
 }

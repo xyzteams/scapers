27e1a90d2e30fc1f24005d2552108e38a89c2ea8
Zayden <adriangota2007@gmail.com>
2024-07-28 16:16:28 +0700

    chore: fix the code

--- src/script/download/youtube.ts
+++ src/script/download/youtube.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
+import yts, { SearchResult, VideoSearchResult } from "yt-search";
 
-interface CobaltResponse {
+interface DownloadResponse {
     [key: string]: any;
 }
 
-function ytmp4(url: string): Promise<CobaltResponse> {
+interface VideoDetails {
+    title: string;
+    url: string;
+    thumbnail: string;
+    duration: {
+        seconds: number;
+        timestamp: string;
+    };
+    views: number;
+    publish: string;
+}
+
+function ytmp4(url: string): Promise<DownloadResponse> {
     return new Promise(async (resolve, reject) => {
         try {
             const response = await axios.post("https://api.cobalt.tools/api/json", {
@@ -24,7 +37,7 @@ function ytmp4(url: string): Promise<CobaltResponse> {
     });
 }
 
-function ytmp3(url: string): Promise<CobaltResponse> {
+function ytmp3(url: string): Promise<DownloadResponse> {
     return new Promise(async (resolve, reject) => {
         try {
             const response = await axios.post("https://api.cobalt.tools/api/json", {
@@ -45,4 +58,26 @@ function ytmp3(url: string): Promise<CobaltResponse> {
     });
 }
 
-export { ytmp4, ytmp3 };
+function search(query: string): Promise<VideoDetails[]> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const response: SearchResult = await yts(query);
+            const result: VideoDetails[] = response.videos.map((video: VideoSearchResult) => ({
+                title: video.title,
+                url: video.url,
+                thumbnail: video.image,
+                duration: {
+                    seconds: video.seconds,
+                    timestamp: video.timestamp,
+                },
+                views: video.views,
+                publish: video.ago,
+            }));
+            resolve(result);
+        } catch (err) {
+            reject(err);
+        }
+    });
+}
+
+export { ytmp4, ytmp3, search };
